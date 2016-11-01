e995b24386f0f4e6b9479825ce74426154918bba
Nathalia Irokawa Fernandes <nathalia.iro.fernandes@gmail.com>
2016-11-01 14:56:56 -0200

    Adicionado SelectList - Dropdown

---
+++ Fiap.Exemplo01.MVC.Web/Controllers/ClienteController.cs
@@ -0,0 +1,40 @@
+﻿using Fiap.Exemplo01.MVC.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo01.MVC.Web.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private static List<Cliente> _lista = new List<Cliente>();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            var lista = new List<string>();
+            lista.Add("Solteiro");
+            lista.Add("Casado");
+            lista.Add("Separado");
+
+            ViewBag.estados = new SelectList(lista);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Cliente cliente)
+        {
+            _lista.Add(cliente);
+            TempData["msg"] = "Cliente Cadastrado";
+            return RedirectToAction("Cadastrar"); //Redirect dentro do servidor
+        }
+
+        public ActionResult Listar()
+        {
+            // ViewBag.lista = _lista; // 1º Opção
+            return View(_lista); // 2º Opção
+        }
+    }
+}
---
+++ Fiap.Exemplo01.MVC.Web/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo01.MVC.Web.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public decimal Credito { get; set; }
+
+        [Display(Name = "Possui necessidades especiais?")]
+        public bool NecessidadesEspeciais { get; set; }
+
+        [Display(Name = "Data de Nascimento")]
+        public DateTime DataNascimento { get; set; }
+
+        [Display(Name = "Estado Civil")]
+        public string EstadoCivil { get; set; }
+    }
+}
